8c685d0321901da65995ea45364881de33c98f29
caliwon <kevin@yorkg.com>
2019-05-07 17:23:21 +0800

    add hw2

---
+++ hw2.py
@@ -0,0 +1,18 @@
+#已知你有一个神奇的list ViaX
+ViaX = ['aaaaa                aaaa', 'aa  aaa            aa  aa', 'aa   aa           aa   aa     aaa    aaa              aaa   aaa', 'aa    aaaaaaaaaaaaa    aa     aaa   aaa  aaa           aaa aaa', 'aa                     aa     aaa   aaa aaaa           aaa aaa', 'aa                     aa      aaa  aaa                 aaaaa', 'aa                     aa      aaa aaa   aaa  aaaaaa     aaaa', 'aa                     aa      aaa aaa   aaa aaa  aaa   aaaaa', 'aa                     aa       aaaaaa   aaa   aaaaaa   aaaaa', 'aa                     aa       aaaaa    aaa aaaaaaaa  aaa aii', 'aa   aa           aa   aa       aaaaa    aaa aaa aaaa  aaa iii', 'aa         aaa         aa        aaaa    aaa  aaaaaaa aaa   iii', '  aa        a        aaa', '    aaaaaaaaaaaaaaaaa']
+'''
+你要做的(To Do): 
+    1. 判断下x的长度
+    2. 观察x里面每一个值的长度，并将结果放入一个新的列表newlist中
+    3. 将x的结果一行行print出来
+'''
+#1. TODO...
+print(len(ViaX))
+#2. TODO...
+newlist = []
+for i in ViaX:
+    newlist.append(len(i))
+print(newlist)
+#3. TODO...
+for i in ViaX:
+    something difference
